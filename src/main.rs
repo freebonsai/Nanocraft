309ec63ae5430a267bffd1dedff908d90aa57682
stivais <stivaisss@gmail.com>
2024-06-03 18:23:57 +0300

    started implementing nanovg alternative

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+mod renderer;
+
 const WINDOW_TITLE: &str = "Nanocraft";
 
 extern crate glfw;
 
+extern crate gl;
+
+extern crate femtovg;
+
 use glfw::{Action, Context, Key};
+use femtovg::renderer::OpenGl;
+use femtovg::{Color};
+use crate::renderer::Renderer;
 
 // https://github.com/rust-tutorials/learn-opengl/blob/main/examples/000-basic-window.rs
 
@@ -11,15 +20,24 @@ fn main() {
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
     // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw.create_window(300, 300, "Nanocraft", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw.create_window(1200, 800, "Nanocraft", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
-    // Make the window's context current
     window.make_current();
     window.set_key_polling(true);
+    glfw.set_swap_interval(glfw::SwapInterval::None);
+
+    let opengl = unsafe { OpenGl::new_from_function(|s| window.get_proc_address(s) as *const _) }.unwrap();
+    let renderer = &mut Renderer::create(opengl);
 
     // Loop until the user closes the window
     while !window.should_close() {
+        let (width, height) = window.get_size();
+        let w = width as u32;
+        let h = height as u32;
+
+        draw(renderer, w, h);
+
         // Swap front and back buffers
         window.swap_buffers();
 
@@ -30,9 +48,17 @@ fn main() {
             match event {
                 glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
                     window.set_should_close(true)
-                },
-                _ => {},
+                }
+                _ => {}
             }
         }
     }
+}
+
+fn draw(renderer: &mut Renderer, w: u32, h: u32) {
+    renderer.begin_frame(w, h);
+
+    renderer.rect(0.0, 0.0, 100.0, 100.0, Color::rgb(255, 0, 0));
+
+    renderer.end_frame();
 }
---
+++ src/renderer.rs
@@ -0,0 +1,33 @@
+use femtovg::{Canvas, Color, Paint, Path};
+use femtovg::renderer::OpenGl;
+
+
+pub struct Renderer {
+    canvas: Canvas<OpenGl>
+}
+
+// unsure if this should be "permanent"
+impl Renderer {
+
+    pub fn create(opengl: OpenGl) -> Renderer {
+        let mut canvas = Canvas::new(opengl).expect("Cannot create canvas");
+        Renderer {
+            canvas
+        }
+    }
+
+    pub fn begin_frame(&mut self, w: u32, h: u32) {
+        self.canvas.set_size(w, h, 1.0);
+        self.canvas.clear_rect(0, 0, w, h, Color::rgb(255, 255, 255));
+    }
+
+    pub fn end_frame(&mut self) {
+        self.canvas.flush()
+    }
+
+    pub fn rect(&mut self, x: f32, y: f32, w: f32, h: f32, color: Color) {
+        let mut path = Path::new();
+        path.rect(x, y, w, h);
+        self.canvas.fill_path(&path, &Paint::color(color));
+    }
+}
